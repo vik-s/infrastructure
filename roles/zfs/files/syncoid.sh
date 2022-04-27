cfcd8bbfc7249ed1950ac0f4a3d968df2aa94519
vik-s <github@vsekar.com>
2022-04-26 20:34:00 -0700

    add healthchecks

--- roles/zfs/files/syncoid.sh
+++ roles/zfs/files/syncoid.sh
@@ -4,4 +4,4 @@
 # Backup docker dataset
 /usr/sbin/syncoid -F --skip-parent --no-sync-snap --sshkey /home/ansible/.ssh/server_privkey ansible@192.168.1.210:rpool/docker tank/docker
 # Call uptime kuma
-/usr/bin/curl -m 10 --retry 3 "https://status.iyer.app/api/push/55E2L8cPsl?msg=OK&ping="
+/usr/bin/curl -m 10 --retry 3 "{{ hcid_syncoid }}"
